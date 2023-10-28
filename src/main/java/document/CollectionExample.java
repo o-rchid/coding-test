23a52f7731ef816950592dd854e3f5d8a5bf46ac
o-rchid <ozzve35@gmail.com>
2023-10-28 23:57:59 +0900

    Collection

---
+++ src/main/java/document/CollectionExample.java
@@ -0,0 +1,25 @@
+package document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionExample {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+        List<String> compareList = new ArrayList<>(List.of("b", "c"));
+
+//        list.size();
+//        list.equals(compareList);
+//        list.isEmpty();
+//        list.contains("");
+//        list.clear();
+//        list.remove("a");
+//        list.removeIf(s -> s == "a");
+//        list.addAll(compareList);
+//        list.containsAll(compareList);
+//        list.removeAll(compareList);
+//        list.retainAll(compareList);
+
+    }
+}
