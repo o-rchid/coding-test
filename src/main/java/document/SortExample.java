2c18605f5c93c857e92ded78fb77a7466bbe53bf
o-rchid <ozzve35@gmail.com>
2023-11-05 23:16:57 +0900

    삽입정렬

--- src/main/java/document/SortExample.java
+++ src/main/java/document/SortExample.java
@@ -5,11 +5,16 @@ import java.util.Arrays;
 public class SortExample {
     public static void main(String[] args) {
         int[] arr = {33, 4, 13, 56, 67, 83, 1, 12};
-        selectionSort(arr);
+        //selectionSort(arr);
+        insertionSort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     * 선택정렬
+     * @param arr
+     */
     private static void selectionSort(int[] arr) {
         int idxMin, tmp;
 
@@ -22,6 +27,24 @@ public class SortExample {
             tmp = arr[idxMin];
             arr[idxMin] = arr[i];
             arr[i] = tmp;
+        }
+    }
+
+    /**
+     * 삽입정렬
+     * @param arr
+     */
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = arr[i];
+            int aux = i - 1;
+
+            while (aux >= 0 && arr[aux] > tmp) {
+                arr[aux + 1] = arr[aux];
+                aux--;
+            }
+
+            arr[aux + 1] = tmp;
         }
     }
 }
