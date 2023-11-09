5ce189f2c9cff4b718d83103b0ad44b416e8fb6c
o-rchid <ozzve35@gmail.com>
2023-11-10 00:09:54 +0900

    피보나치

--- src/main/java/document/DynamicProgramming.java
+++ src/main/java/document/DynamicProgramming.java
@@ -3,7 +3,7 @@ package document;
 public class DynamicProgramming {
 
     public static void main(String[] args) {
-        System.out.println(fib(3));
+        System.out.println(fibo(3));
     }
     public static int fib(int n) {
         if (n == 0 || n == 1) {
@@ -12,16 +12,18 @@ public class DynamicProgramming {
         return fib(n - 1) + fib(n - 2);
     }
 
-    public static long[] memo;
+    public static long[] memo = new long[100];
 
     public static long fibo(int num) {
         if (num == 0 || num == 1) {
             return num;
-        } else if (memo[num] != 0) {
+        }
+        else if (memo[num] != 0) {
             return memo[num];
         }
         else{
-            memo[num] = fibo(num -2)
+            memo[num] = fibo(num - 2) + fibo(num - 1);
+            return memo[num];
         }
-        }
+    }
 }
