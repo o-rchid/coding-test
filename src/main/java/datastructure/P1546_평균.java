a521e73837b785c62f88fd7dc2af445dae3cf809
o-rchid <ozzve35@gmail.com>
2023-12-09 23:46:57 +0900

    -03

--- src/main/java/datastructure/P1546_평균.java
+++ src/main/java/datastructure/P1546_평균.java
@@ -11,12 +11,15 @@ public class P1546_평균 {
         for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
+
         long sum = 0;
         long max = 0;
+
         for (int i = 0; i < N; i++) {
             if (A[i] > max) max = A[i];
             sum += A[i];
         }
+
         System.out.println(sum * 100.0 / max / N);
     }
 }
