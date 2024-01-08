73063f2bc95a041dfc9fd2887ad0b3bafe88454a
o-rchid <ozzve35@gmail.com>
2024-01-08 23:20:56 +0900

    알고리즘

---
+++ src/main/java/timecomplexity/B14262_알고리즘수업1.java
@@ -0,0 +1,9 @@
+package timecomplexity;
+
+public class B14262_알고리즘수업1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+
+}
---
+++ src/main/java/timecomplexity/B24263_알고리즘수업2.java
@@ -0,0 +1,14 @@
+package timecomplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B24263_알고리즘수업2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(n);
+        System.out.println('1');
+    }
+}
